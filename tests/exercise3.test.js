1c06cefd3e7b8e67cc72c170fdac41b10dc8621b
M-Saleh-N <salehnasser65784@gmail.com>
2025-01-29 14:36:31 +0300

    tested : GREEN

--- tests/exercise3.test.js
+++ tests/exercise3.test.js
@@ -3,4 +3,4 @@ const biggestNUmber = require('../src/exercise3');
 
 test("returns the largest number in the array" , () => {
     expect(biggestNUmber([456,987,222, 455, 678, 991])).toBe(991);
-})
+}) 
