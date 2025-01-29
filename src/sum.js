09e166605e9a2498bb3288f55436cb0c52b915a1
M-Saleh-N <salehnasser65784@gmail.com>
2025-01-29 13:31:44 +0300

    added functins

--- src/sum.js
+++ src/sum.js
@@ -2,4 +2,19 @@ function sum(a,b) {
     return a + b
 }
 
-module.exports = sum
+function subtract(a,b){
+    return a - b 
+}
+
+const multiply = (a,b) => a * b
+
+function divide(a,b){
+    return a / b 
+}
+
+module.exports = {
+    sum,
+    subtract,
+    multiply,
+    divide
+}
