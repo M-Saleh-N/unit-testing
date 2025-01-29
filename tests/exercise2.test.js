34f35d4455fdf2a3b9e2bf1fe7dc09abf922ccf6
M-Saleh-N <salehnasser65784@gmail.com>
2025-01-29 14:24:26 +0300

    added another assertion

--- tests/exercise2.test.js
+++ tests/exercise2.test.js
@@ -4,3 +4,7 @@ test("it returns true if the number is divisible by 2" , () => {
     expect(isDivisbleByTwo (6)).toBe(true);
 })
 
+test("it returns false if the number is not divisble by 2" , () => {
+    expect(isDivisbleByTwo (5)).toBe(false);
+
+})
