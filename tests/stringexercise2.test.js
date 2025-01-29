df1bb147951ff565b1d7faf91eb0fa1968b92bf4
M-Saleh-N <salehnasser65784@gmail.com>
2025-01-29 14:36:00 +0300

    deleted

--- tests/stringexercise2.test.js
+++
@@ -1,8 +0,0 @@
-const schoolName = require("../src/stringexercise1")
-
-test("it concatinates two strings", () => {
-    // assertion
-    //this is used to very that the functions outputs the required sum in our case 
-    expect(schoolName("Rift","Koders")).toBe("Rift, Koders")
-    expect(schoolName("Good","Morning")).toBe("Good, Morning")
-})
